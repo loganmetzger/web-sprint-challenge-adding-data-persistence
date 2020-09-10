dccdc12886ba653669a494ecd26fc4fc9ea1c36f
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-09-10 16:14:48 -0400

    prompted server to use routers, have not created router endpoints yet

--- api/server.js
+++ api/server.js
@@ -1,12 +1,18 @@
 const express = require('express')
-
 const server = express()
 
-
-const db = require('../connection')
-
-
-
 server.use(express.json())
+
+const projectsRouter = require()
+const tasksRouter = require()
+const resourcesRouter = require()
+
+server.use('/api/projects', projectsRouter)
+server.use('/api/tasks', tasksRouter)
+server.use('/api/resources', resourcesRouter)
+
+server.get('/', (req, res) => {
+  res.send('GET request to the homepage')
+})
 
 module.exports = server;
